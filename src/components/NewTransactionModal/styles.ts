0e4eb34585800b919b7e1c830a964b536e106ead
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-18 00:45:22 -0300

    botões de entrada e saidas acessíveis

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as RadioGroup from '@radix-ui/react-radio-group';
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -14,7 +15,6 @@ export const Content = styled(Dialog.Content)`
   border-radius: 6px;
   padding: 2.5rem 3rem;
   background: ${(props) => props.theme['gray-800']};
-
   position: fixed;
   top: 50%;
   left: 50%;
@@ -22,38 +22,36 @@ export const Content = styled(Dialog.Content)`
 
   form {
     margin-top: 2rem;
-
     display: flex;
     flex-direction: column;
     gap: 1rem;
 
-    input: {
+    input {
       border-radius: 6px;
       border: 0;
       background: ${(props) => props.theme['gray-900']};
       color: ${(props) => props.theme['gray-300']};
       padding: 1rem;
-
       &::placeholder {
         color: ${(props) => props.theme['gray-500']};
       }
     }
-  }
 
-  button[type='submit'] {
-    height: 58px;
-    border: 0;
-    background-color: ${(props) => props.theme['green-500']};
-    color: ${(props) => props.theme.white};
-    font-weight: bold;
-    padding: 0 1.25rem;
-    border-radius: 6px;
-    margin-top: 1.5rem;
-    cursor: pointer;
+    button[type='submit'] {
+      height: 50px;
+      border: 0;
+      background: ${(props) => props.theme['green-500']};
+      color: ${(props) => props.theme.white};
+      font-weight: bold;
+      padding: 0 1.25rem;
+      border-radius: 6px;
+      margin-top: 1.25rem;
+      cursor: pointer;
 
-    &:hover {
-      background-color: ${(props) => props.theme['green-700']};
-      transition: background-color 0.2s;
+      &:hover {
+        background: ${(props) => props.theme['green-700']};
+        transition: background-color 0.2s;
+      }
     }
   }
 `;
@@ -67,4 +65,54 @@ export const CloseButton = styled(Dialog.Close)`
   cursor: pointer;
   color: ${(props) => props.theme['gray-300']};
   line-height: 0;
+`;
+
+export const TransactionType = styled(RadioGroup.Root)`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(2, 1fr);
+  margin-top: 0.5rem;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome';
+}
+
+export const TransactionTypeButton = styled(
+  RadioGroup.Item
+)<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme['gray-700']};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${(props) => props.theme['gray-300']};
+
+  svg {
+    color: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-300']
+        : props.theme['red-300']};
+  }
+
+  &[data-state='unchecked']:hover {
+    transition: background-color 0.2s;
+    background: ${(props) => props.theme['gray-600']};
+  }
+
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-500']
+        : props.theme['red-500']};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
+  }
 `;
