172562d393811df0765337eb0a374210062f0fcc
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-17 23:54:10 -0300

    componente header, summary, searchform - tabela de transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
+import { Transactions } from './pages/Transactions';
 
 function App() {
-  return;
-  <ThemeProvider theme={defaultTheme}>
-    <GlobalStyle />
-    <div>Teste</div>
-  </ThemeProvider>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Transactions />
+    </ThemeProvider>
+  );
 }
 
 export default App;
